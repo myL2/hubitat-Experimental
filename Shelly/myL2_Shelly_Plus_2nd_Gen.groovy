c0d4ea1160bc2ce4673d663bb1a784ac63480ac2
myL2 <116822919+myL2@users.noreply.github.com>
2023-05-07 12:41:57 +0300

    Update myL2_Shelly_Plus_2nd_Gen.groovy

--- Shelly/myL2_Shelly_Plus_2nd_Gen.groovy
+++ Shelly/myL2_Shelly_Plus_2nd_Gen.groovy
@@ -27,292 +27,87 @@ import org.apache.commons.codec.binary.Base64
 
 metadata {
     definition (
-        name: "Shelly Plus 2nd Gen",
+        name: "Shelly Plus 2nd Gen Child",
         namespace: "myL2",
         author: "SebyM"
     )
     {
+        capability "Actuator"
+        capability "EnergyMeter"
+        capability "Sensor"
         capability "Refresh"
+        capability "Switch"
+        capability "RelaySwitch"
+        capability "PowerMeter"
+        capability "EnergyMeter"
         capability "SignalStrength"
         capability "TemperatureMeasurement"
         capability "VoltageMeasurement"
-        capability "Polling"
 
-        attribute "StableFW_Update", "string"
-        attribute "LastRefresh", "string"
-        attribute "power", "number"
-        attribute "overpower", "string"
-        attribute "DeviceOverTemp", "string"
-        attribute "MAC", "string"
-        attribute "Primary_IP", "string"
-        attribute "Primary_SSID", "string"
-        attribute "Secondary_IP", "string"
-        attribute "Secondary_SSID", "string"
-        attribute "WiFiSignal", "string"
-        attribute "Cloud", "string"
-        attribute "Cloud_Connected", "string"
-        attribute "energy", "number"
-        attribute "DeviceType", "string"
-        attribute "eMeter", "number"
-        attribute "reactive", "number"
-        attribute "MaxPower", "number"
-        attribute "CircuitAmp", "string"
-        attribute "LED_Output", "string"
-        attribute "LED_NetworkStatus", "string"
-        attribute "DeviceName", "string"
-        attribute "RelayName", "string"
-        attribute "Eco_Mode", "string"
-
-        command "RebootDevice"
-        command "UpdateDeviceFW"
-        command "configureChildren"
-        command "removeChildren"
-        command "testCmd"
+        attribute "lastPowerUpdate", "number"
+        attribute "lastPowerValue", "number"
+        attribute "energyExact", "number"
+        
+        command "resetEnergy"
     }
 
     preferences {
-        def refreshRate = [:]
-        refreshRate << ["1 min" : "Refresh every minute"]
-        refreshRate << ["5 min" : "Refresh every 5 minutes"]
-        refreshRate << ["15 min" : "Refresh every 15 minutes"]
-        refreshRate << ["30 min" : "Refresh every 30 minutes"]
-        refreshRate << ["manual" : "Manually or Polling Only"]
-
         input("ip", "string", title:"IP", description:"Shelly IP Address", defaultValue:"" , required: true)
+        input("channel", "number", title:"Channel", description:"Child device Channel" , required: true)
         input name: "username", type: "text", title: "Username:", description: "(blank if none)", required: false
         input name: "password", type: "password", title: "Password:", description: "(blank if none)", required: false
-        input("channels", "number", title:"Number of channels", description:"1,2,3,4", defaultValue:"0" , required: true)
-        input("refresh_Rate", "enum", title: "Device Refresh Rate", description:"<font color=red>!!WARNING!!</font><br>DO NOT USE if you have over 50 Shelly devices.", options: refreshRate, defaultValue: "manual")
-
-        input name: "debugOutput", type: "bool", title: "Enable debug logging?", defaultValue: false
-        input name: "debugParse", type: "bool", title: "Enable JSON parse logging?", defaultValue: false
-        input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
     }
 }
 
 def initialize() {
-    log.info "Shelly Plus 2nd Gen/Parent/initialize"
-    if (txtEnable) log.info "initialize"
+    log.info "Shelly Plus 2nd Gen/Child/Initialize"
+    resetEnergy()
 }
 
 def installed() {
-    log.debug "Shelly Plus 2nd Gen/Parent/Installed"
-    state.DeviceName = "NotSet"
-    state.RelayName = "NotSet"
+    log.debug "Shelly Plus 2nd Gen/Child/Installed"
+    resetEnergy()
 }
 
 def uninstalled() {
     unschedule()
-    log.debug "Shelly Plus 2nd Gen/Parent/Uninstalled"
+    log.debug "Shelly Plus 2nd Gen/Child/Uninstalled"
 }
 
 def updated() {
-    if (txtEnable) log.info "Preferences updated..."
-    log.warn "Debug logging is: ${debugOutput == true}"
-    log.warn "Switch protection is: ${settings?.protect}"
-    unschedule()
-    dbCleanUp()
-
-    switch(refresh_Rate) {
-        case "1 min" :
-        runEvery1Minute(autorefresh)
-        break
-        case "5 min" :
-        runEvery5Minutes(autorefresh)
-        break
-        case "15 min" :
-        runEvery15Minutes(autorefresh)
-        break
-        case "30 min" :
-        runEvery30Minutes(autorefresh)
-        break
-        case "manual" :
-        unschedule(autorefresh)
-        log.info "Autorefresh disabled"
-        break
-    }
-    if (txtEnable) log.info ("Auto Refresh set for every ${refresh_Rate} minute(s).")
-
-    if (debugOutput) runIn(1800,logsOff)
-    if (debugParse) runIn(300,logsOff)
-
-    state.LastRefresh = new Date().format("YYYY/MM/dd \n HH:mm:ss", location.timeZone)
-    poll()
-    configureChildren()
-
+    log.info "Preferences updated..."
     refresh()
-}
-
-def configureChildren(){
-    if (channels) {
-        String thisId = device.deviceNetworkId
-        for (int myindex = 0; myindex < channels; myindex++) {
-            if (!getChildDevice("${thisId}-${myindex}")) {
-                newDevice= addChildDevice("myL2", "Shelly Plus 2nd Gen Child", "${thisId}-${myindex}", [name: "${device.label} - Channel ${myindex}", isComponent: true])
-                newDevice.updateSetting("ip",[type:"string", value: ip])
-                newDevice.updateSetting("channel",[type:"number", value: (myindex)])
-                newDevice.updateSetting("username",[type:"string", value: username])
-                newDevice.updateSetting("password",[type:"string", value: password])
-                log.info "Installing child ${thisId}-${myindex}."
-            }
-        }
-    }    
-}
-
-def removeChildren(){
-    if (channels) {
-        String thisId = device.deviceNetworkId
-        for (int myindex = 0; myindex < channels; myindex++) {
-            log.info myindex
-            if (getChildDevice("${thisId}-${myindex}")) {
-                deleteChildDevice("${thisId}-${myindex}")
-                log.info "Removing child ${thisId}-${myindex}."
-            }
-        }
-    }    
-}
-
-def testCmd(){
-    uploadScript()
-}
-
-private dbCleanUp() {
-    state.remove("ver")
-    state.remove("id")
-    state.remove("ShellyfwUpdate")
-    state.remove("power")
-    state.remove("overpower")
-    state.remove("dcpower")
-    state.remove("max_power")
-    state.remove("internal_tempC")
-    state.remove("Status")
-    state.remove("max_power")
-    state.remove("RelayName")
-    state.remove("powerSource")
-    state.remove("has_update")
-}
-
-def parse(values){
-    def original = "{\""+values.replace(":","\":\"").replace(", ","\",\"")+"-\"}"
-    def json = new JsonSlurper().parseText(original)
-    def header = new String(json.headers.decodeBase64())
-    def s = header.split(' ')
-    def ss = s[1].split('/')
-    def channelNo = ss[2]
-    def channelValue = ss[3]
-
-    if (channels) {
-        String thisId = device.deviceNetworkId
-        child = getChildDevice("${thisId}-${channelNo}")
-        if (child) {
-            if(debugOutput) log.debug "${child} is now ${channelValue}"
-            switch (ss[1]){
-                case "switch":
-                child.updateRelayState(channelValue)
-                break;
-                case "power":
-                child.updateRelayPower(channelValue)
-                break;
-            }
-        }else{
-            log.debug "Got a request for a child that does not exist: ${ss}"
-        }
-    }
-
-}
-
-def uploadScript(){
-    def chunks=[
-        [id: 1, append: true, code: "\"let minThreshold=10;\\nlet lastPower=0;\\nlet baseUrl='http://192.168.100.160:39501/';\\nShelly.addStatusHandler(function(event) {\\nif (event.name===\\\"switch\\\"){\\n \""],
-        [id: 1, append: true, code: "\"if(typeof(event.delta.output)!==\\\"undefined\\\"){\\n  let url=baseUrl+'switch/'+JSON.stringify(event.id)+'/'+\""],
-        [id: 1, append: true, code: "\"(event.delta.output?\\\"on\\\":\\\"off\\\")+\\\"/\\\"; Shelly.call(\\\"HTTP.GET\\\", {\\\"url\\\": url});\\n }\\n\""],
-        [id: 1, append: true, code: "\" if (typeof event.delta.apower!==\\\"undefined\\\"){\\n  if (Math.abs(event.delta.apower-lastPower)>minThreshold){\\n   lastPower=event.delta.apower;\\n\""],
-        [id: 1, append: true, code: "\"   let url=baseUrl+'power/'+JSON.stringify(event.id)+\\\"/\\\"+JSON.stringify(event.delta.apower)+\\\"/\\\"; Shelly.call(\\\"HTTP.GET\\\", {\\\"url\\\": url});\\n  }\\n }\\n}});\"\""],
-    ]
-    sendScriptCmd("http://${ip}/rpc/Script.Delete", [id: 1]);
-    sendScriptCmd("http://${ip}/rpc/Script.Create?", [:]);
-    sendScriptCmd("http://${ip}/rpc/Script.PutCode",chunks[0]);
-    sendScriptCmd("http://${ip}/rpc/Script.PutCode",chunks[1]);
-    sendScriptCmd("http://${ip}/rpc/Script.PutCode",chunks[2]);
-    sendScriptCmd("http://${ip}/rpc/Script.PutCode",chunks[3]);
-    sendScriptCmd("http://${ip}/rpc/Script.PutCode",chunks[4]);
-    sendScriptCmd("http://${ip}/rpc/Script.SetConfig",[id: 1, config: "{enable:true, name:\"Hubitat\"}"]);
-    sendScriptCmd("http://${ip}/rpc/Script.Start", [id: 1]);
-    log.debug "Script upload finished"
-}
-
-def sendScriptCmd(uri, args){
-    def params = [
-        uri: uri,
-        requestContentType: 'text/html',
-        query: args
-    ]
-    //log.info params
-    try{
-        httpGet(params) {
-            resp -> resp.headers.each {
-                //log.debug "Response: ${it.name} : ${it.value}"
-            }
-        }
-    }catch (e){
-        log.debug "Err: " + e.message
-        log.debug "Err response: " + e.response.getData()
-    }
 }
 
 def refresh(){
-    log.debug "Shelly Refresh called"
-
-    if (channels) {
-        String thisId = device.deviceNetworkId
-        for (int myindex = 0; myindex < channels; myindex++) {
-            child = getChildDevice("${thisId}-${myindex}")
-            if (child) {
-                child.refresh()
-            }
-        }
-    }
-
-}
-
-def getWiFi(){
-    logDebug "WiFi Status called"
-    def params = [uri: "http://${username}:${password}@${ip}/rpc/wifi.GetStatus"]
+    //log.info "Shelly Refresh called"
+    def params = [uri: "http://${username}:${password}@${ip}/rpc/Switch.GetStatus?id=${channel}"]
 
     try {
         httpGet(params) {
-            resp -> resp.headers.each {
-                logJSON "Response: ${it.name} : ${it.value}"
-            }
+            resp -> resp.headers.each {    }
             obs = resp.data
-            logJSON "params: ${params}"
-            logJSON "response contentType: ${resp.contentType}"
-            logJSON "response data: ${resp.data}"
+            if(obs.voltage != null){
+                sendEvent(name: "voltage", value: obs.voltage)
+            }
 
-            state.rssi = obs.rssi
-            state.ssid = obs.ssid
-            state.ip = obs.sta_ip
+            if (obs.temperature.tC != null){
+                if (state.temp_scale == "C") sendEvent(name: "temperature", value: obs.temperature.tC)
+                if (state.temp_scale == "F") sendEvent(name: "temperature", value: obs.temperature.tF)
+            }
 
-            sendEvent(name: "Primary_SSID", value: state.ssid)
-            sendEvent(name: "Primary_IP", value: state.ip)
+            ison = obs.output
+            if (ison == true) {
+                sendEvent(name: "switch", value: "on")
+            } else {
+                sendEvent(name: "switch", value: "off")
+            }
 
-            /*
--30 dBm Excellent | -67 dBm     Good | -70 dBm  Poor | -80 dBm  Weak | -90 dBm  Dead
-*/
-            signal = state.rssi
-            if (signal <= 0 && signal >= -70) {
-                sendEvent(name:  "WiFiSignal", value: "<font color='green'>Excellent</font>", isStateChange: true);
-            } else
-                if (signal < -70 && signal >= -80) {
-                    sendEvent(name:  "WiFiSignal", value: "<font color='green'>Good</font>", isStateChange: true);
-                } else
-                    if (signal < -80 && signal >= -90) {
-                        sendEvent(name: "WiFiSignal", value: "<font color='yellow'>Poor</font>", isStateChange: true);
-                    } else 
-                        if (signal < -90 && signal >= -100) {
-                            sendEvent(name: "WiFiSignal", value: "<font color='red'>Weak</font>", isStateChange: true);
-                        }
-            sendEvent(name: "rssi", value: state.rssi)
+            if(obs.apower != null){
+                power = obs.apower
+                sendEvent(name: "power", unit: "W", value: power)
+                updateEnergy(power)
+            }
 
         } // End try
     } catch (e) {
@@ -321,177 +116,56 @@ def getWiFi(){
 
 }
 
-def getDeviceInfo(){
+def resetEnergy(){
+    sendEvent(name: "lastPowerValue", value: 0)
+    sendEvent(name: "energy", value: 0)
+    sendEvent(name: "energyExact", value: 0)
+    sendEvent(name: "lastPowerUpdate", value: now())
+}
 
-    logDebug "Sys Status called"
-    //getSettings()
-    def params = [uri: "http://${username}:${password}@${ip}/rpc/Shelly.GetDeviceInfo"]
+def updateRelayState(newState){
+    sendEvent(name: "switch", value: newState)
+}
 
+def updateRelayPower(newPower){
+    sendEvent(name: "power", value: newPower)
+    updateEnergy(Float.parseFloat(newPower))
+}
+
+def on() {
+    sendSwitchCommand "/rpc/Switch.Set?id=${channel}&on=true"
+}
+
+def off() {
+    sendSwitchCommand "/rpc/Switch.Set?id=${channel}&on=false"
+}
+
+def sendSwitchCommand(action) {
+    def params = [uri: "http://${username}:${password}@${ip}/${action}"]
     try {
         httpGet(params) {
             resp -> resp.headers.each {
-                logJSON "Response: ${it.name} : ${it.value}"
             }
-            obs = resp.data
-            logJSON "params: ${params}"
-            logJSON "response contentType: ${resp.contentType}"
-            logJSON "response data: ${resp.data}"
-
-            state.mac = obs.mac
-            
-            device.deviceNetworkId = state.mac.replace(":","").toUpperCase()
-
-            updateDataValue("FW Version", obs.ver)
-            updateDataValue("model", obs.model)
-            updateDataValue("ShellyHostname", obs.id)
-            updateDataValue("Device Type", obs.app)
-
-        } // End try
-    } catch (e) {
-        log.error "something went wrong: $e"
-    }
-
-}
-
-def getGetConfig(){
-
-    logDebug "Sys Status called"
-    //getSettings()
-    def params = [uri: "http://${username}:${password}@${ip}/rpc/Sys.GetConfig"]
-
-    try {
-        httpGet(params) {
-            resp -> resp.headers.each {
-                logJSON "Response: ${it.name} : ${it.value}"
-            }
-            obs = resp.data
-            logJSON "params: ${params}"
-            logJSON "response contentType: ${resp.contentType}"
-            logJSON "response data: ${resp.data}"
-
-            sendEvent(name: "Eco_Mode", value: obs.device.eco_mode)
-
-        } // End try
-    } catch (e) {
-        log.error "something went wrong: $e"
-    }
-
-}
-
-def CheckForUpdate() {
-    if (txtEnable) log.info "Check Device FW"
-    def params = [uri: "http://${username}:${password}@${ip}/rpc/Shelly.CheckForUpdate"]
-    try {
-        httpGet(params) {
-            resp -> resp.headers.each {
-                logDebug "Response: ${it.name} : ${it.value}"
-            }
-            obs = resp.data
-            response = "${obs.toString()}"
-            logJSON "params: ${params}"
-            logJSON "response contentType: ${resp.contentType}"
-            logJSON "response data: ${resp.data}"
-
-            if(response.contains("stable")) {
-                sendEvent(name:  "StableFW_Update", value: "<font color='green'>Available</font>", isStateChange: true);
-            }else
-                if(!(response.contains("stable"))) {
-                    sendEvent(name:  "StableFW_Update", value: "Current", isStateChange: true);
-                }
-
-        } // End try
-
+        } 
     } catch (e) {
         log.error "something went wrong: $e"
     }
 }
 
 
-def ping() {
-    logDebug "ping"
-    poll()
+def setEnergy(float energy) {
+    sendEvent(name: "energyExact", value: energy)
+    sendEvent(name: "energy", value: Math.round(energy*100)/100)
 }
 
-def logsOff(){
-    log.warn "debug logging auto disabled..."
-    device.updateSetting("debugOutput",[value:"false",type:"bool"])
-    device.updateSetting("debugParse",[value:"false",type:"bool"])
-}
-
-def autorefresh() {
-    if (locale == "UK") {
-        logDebug "Get last UK Date DD/MM/YYYY"
-        state.LastRefresh = new Date().format("d/MM/YYYY \n HH:mm:ss", location.timeZone)
-        sendEvent(name: "LastRefresh", value: state.LastRefresh, descriptionText: "Last refresh performed")
-    } 
-    if (locale == "US") {
-        logDebug "Get last US Date MM/DD/YYYY"
-        state.LastRefresh = new Date().format("MM/d/YYYY \n HH:mm:ss", location.timeZone)
-        sendEvent(name: "LastRefresh", value: state.LastRefresh, descriptionText: "Last refresh performed")
-    }
-    //if (txtEnable) log.info "Executing 'auto refresh'" //RK
-    refresh()
-}
-
-private logJSON(msg) {
-    if (settings?.debugParse || settings?.debugParse == null) {
-        log.info "$msg"
-    }
-}
-
-private logDebug(msg) {
-    if (settings?.debugOutput || settings?.debugOutput == null) {
-        log.debug "$msg"
-    }
-}
-
-//RK Updated to include last refreshed
-def poll() {
-    if (locale == "UK") {
-        logDebug "Get last UK Date DD/MM/YYYY"
-        state.LastRefresh = new Date().format("d/MM/YYYY \n HH:mm:ss", location.timeZone)
-        sendEvent(name: "LastRefresh", value: state.LastRefresh, descriptionText: "Last refresh performed")
-    } 
-    if (locale == "US") {
-        logDebug "Get last US Date MM/DD/YYYY"
-        state.LastRefresh = new Date().format("MM/d/YYYY \n HH:mm:ss", location.timeZone)
-        sendEvent(name: "LastRefresh", value: state.LastRefresh, descriptionText: "Last refresh performed")
-    }
-    if (txtEnable) log.info "Polling"
-    getWiFi()
-    getDeviceInfo()
-    getGetConfig()
-    CheckForUpdate()
-}
-
-def RebootDevice() {
-    if (txtEnable) log.info "Rebooting Device"
-    def params = [uri: "http://${username}:${password}@${ip}/rpc/Shelly.Reboot"]
-    try {
-        httpGet(params) {
-            resp -> resp.headers.each {
-                logDebug "Response: ${it.name} : ${it.value}"
-            }
-        } // End try
-
-    } catch (e) {
-        log.error "something went wrong: $e"
-    }
-    runIn(15,refresh)
-}
-
-def UpdateDeviceFW() {
-    if (txtEnable) log.info "Updating Device FW"
-    def params = [uri: "http://${username}:${password}@${ip}/rpc/Shelly.Update?stage=stable"]
-    try {
-        httpGet(params) {
-            resp -> resp.headers.each {
-                logDebug "Response: ${it.name} : ${it.value}"
-            }
-        } // End try
-
-    } catch (e) {
-        log.error "something went wrong: $e"
-    }
-    runIn(30,refresh)
+def updateEnergy(float power) {
+    r = (now() - device.currentValue("lastPowerUpdate")) / 1000
+    p = device.currentValue("lastPowerValue")
+    energy = device.currentValue("energyExact")
+    newEnergy = (energy + (p/1000/60/60*r))
+    log.debug "VEM: $r time passed, power was $power, lastPower was $p, energy was $energy, newEnergy is $newEnergy"
+    sendEvent(name: "energyExact", value: newEnergy)
+    sendEvent(name: "energy", value: Math.round(newEnergy*100)/100)
+    sendEvent(name: "lastPowerValue", value: power)
+    sendEvent(name: "lastPowerUpdate", value: now())
 }
